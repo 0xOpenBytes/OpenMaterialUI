b8b4c6ea561f98fb4794643db10f21e8cf9b7c94
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-05-16 23:01:43 +0300

    package init

---
+++ Sources/OpenMaterialUI/OpenMaterialUI.swift
@@ -0,0 +1,6 @@
+public struct OpenMaterialUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/OpenMaterialUITests/OpenMaterialUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import OpenMaterialUI
+
+final class OpenMaterialUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(OpenMaterialUI().text, "Hello, World!")
+    }
+}
