cafc02e5a198c235f202470d6a066267f314e79c
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-05-16 23:13:01 +0300

    Add iOS 16.0 platform support

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "OpenMaterialUI",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OpenMaterialUI",
-            targets: ["OpenMaterialUI"]),
+            targets: ["OpenMaterialUI"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OpenMaterialUI",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "OpenMaterialUITests",
-            dependencies: ["OpenMaterialUI"]),
+            dependencies: ["OpenMaterialUI"]
+        ),
     ]
 )
