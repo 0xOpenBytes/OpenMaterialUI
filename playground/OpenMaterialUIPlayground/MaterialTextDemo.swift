b404253bac7a1e912659683598a4790ae97c0d30
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-10-03 08:18:25 +0300

    update: clean Package.swift

---
+++ playground/OpenMaterialUIPlayground/MaterialTextDemo.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialTextDemo.swift
+//  OpenMaterialUIPlayground
+//
+//  Created by Ahmed Shendy on 07/07/2023.
+//
+
+import SwiftUI
+
+struct MaterialTextDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MaterialTextDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        MaterialTextDemo()
+    }
+}
